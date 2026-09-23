ac03aaf53c991feaa4511a6041459421a28b33ac
Your Name <70196039+aqduswarsi@users.noreply.github.com>
2026-09-23 11:57:05 +0530

    Update events

--- models/Event.js
+++ models/Event.js
@@ -3,13 +3,8 @@ const mongoose = require("mongoose");
 const eventSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    date: { type: String, default: "" },
-    time: { type: String, default: "" },
-    location: { type: String, default: "" },
     description: { type: String, default: "" },
-    image: { type: String, default: "" }, // base64 ya URL
-    category: { type: String, default: "General" }, // General/Training/Recruitment/Meeting
-    status: { type: String, default: "upcoming" }, // upcoming/ongoing/completed
+    date: { type: String, default: "" },
   },
   { timestamps: true },
 );
